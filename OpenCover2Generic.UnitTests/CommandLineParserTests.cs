edd137d7160f16c22b012a8185433aea289d7d99
Peter Stevens <peter.stevens@bakerhughes.com>
2017-10-10 19:12:23 +0200

    Buildng test for commandlineparser

---
+++ OpenCover2Generic.UnitTests/CommandLineParserTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    [TestClass]
+    public class CommandLineParserTests
+    {
+        [TestMethod]
+        public void TwoArgmentsShouldParse()
+        {
+            string[] args= { "-OpenCover:SomeFile.xml", "-Generic:SomeResult.xml" };
+            ICommandLineParser = new CommandLineParserTests();
+        }
+
+    }
+}
