d4ac467936e7f69256ce807eefa46b8fbacb3023
Peter Stevens <peter.stevens@bakerhughes.com>
2017-10-14 11:36:40 +0200

    Test Passes

--- OpenCover2Generic.UnitTests/BranchPointsMultipeTimesCoveredTests.cs
+++ OpenCover2Generic.UnitTests/BranchPointsMultipeTimesCoveredTests.cs
@@ -7,11 +7,17 @@ namespace BHGE.SonarQube.OpenCover2Generic
     public class BranchPointsMultipeTimesCoveredTests
     {
         [TestMethod]
-        public void TheUltimate()
+        public void PointWithPathVisitedExpectOneVisited()
         {
             IBranchPoint point = new BranchPoint(0, true);
             Assert.AreEqual(1, point.PathsVisited);
+        }
 
+        [TestMethod]
+        public void PointWithThreePathsTwoVisitedExpectTwoVisited()
+        {
+            IBranchPoint point = new BranchPoint(0, true).Add(new BranchPoint(1, false)).Add(new BranchPoint(2, true));
+            Assert.AreEqual(2, point.PathsVisited);
         }
     }
 }
