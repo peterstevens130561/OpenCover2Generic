6185d8b2c64e3bae899afa522934d0bf5a27b39c
Peter Stevens <peter.stevens@bakerhughes.com>
2017-10-21 14:33:42 +0200

    Building commandLineParser

---
+++ OpenCover2Generic.Runner/OpenCoverWrapperCommandLineParser.cs
@@ -0,0 +1,19 @@
+﻿using BHGE.SonarQube.OpenCover2Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGE.SonarQube.OpenCover2Generic.OpenCoverRunner
+{
+    class OpenCoverWrapperCommandLineParser
+    {
+        private readonly ICommandLineParser _commandLineParser;
+
+        public OpenCoverWrapperCommandLineParser(ICommandLineParser commandLineParser)
+        {
+            _commandLineParser = commandLineParser;
+        }
+    }
+}
