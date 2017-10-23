01800a4d7e01933921c0fba4d71e5ae3dc728d81
Peter Stevens <peter.stevens@bakerhughes.com>
2017-10-23 17:01:11 -0600

    OpenCoverCoverageWriter deals with empty doc

--- OpenCover2Generic.Converter/ICoverageWriter.cs
+++ OpenCover2Generic.Converter/ICoverageWriter.cs
@@ -4,7 +4,16 @@ namespace BHGE.SonarQube.OpenCover2Generic
 {
     public interface ICoverageWriter
     {
+        /// <summary>
+        /// This is where you create all of the header stuff + root element
+        /// </summary>
+        /// <param name="xmlWriter"></param>
         void WriteBegin(XmlTextWriter xmlWriter);
+
+        /// <summary>
+        /// Write the end element & flush
+        /// </summary>
+        /// <param name="xmlWriter"></param>
         void WriteEnd(XmlWriter xmlWriter);
 
         void GenerateCoverage(IModel model, XmlWriter xmlWriter);
---
+++ OpenCover2Generic.Converter/OpenCoverCoverageWriter.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System;
+using System.Xml;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    public class OpenCoverCoverageWriter : ICoverageWriter
+    {
+        public void GenerateCoverage(IModel model, XmlWriter xmlWriter)
+        {
+            if(model.GetCoverage().Count ==0)
+            {
+                return;
+            }
+            xmlWriter.WriteStartElement("Modules");
+            xmlWriter.WriteStartElement("Module");
+            WriteFilesElement(model, xmlWriter);
+
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndElement();
+        }
+
+        private static void WriteFilesElement(IModel model, XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartElement("Files");
+            foreach (IFileCoverageModel fileCoverage in model.GetCoverage())
+            {
+                //<File uid="1" fullPath="E:\Cadence\EsieTooLinkRepositoryServiceTest.cs" />
+                xmlWriter.WriteStartElement("File");
+                //xmlWriter.WriteAttributeString("uid", fileCoverage.Id);
+                xmlWriter.WriteAttributeString("fullPath", fileCoverage.FullPath);
+                xmlWriter.WriteEndElement();
+            }
+            xmlWriter.WriteEndElement();
+        }
+
+        public void WriteBegin(XmlTextWriter xmlWriter)
+        {
+            xmlWriter.Formatting = Formatting.Indented;
+            xmlWriter.Indentation = 4;
+            xmlWriter.WriteStartDocument();
+            //xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"
+            xmlWriter.WriteStartElement("CoverageSession");
+            xmlWriter.WriteAttributeString("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+            xmlWriter.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        }
+
+        public void WriteEnd(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Flush();
+        }
+    }
+}
