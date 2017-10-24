2e145eb2e4155ffe8afa26d837ddc3ee41146042
Peter Stevens <peter.stevens@bakerhughes.com>
2017-10-24 01:58:25 -0700

    Even here a bit of cleanup

--- OpenCover2Generic.Converter/MultiAssemblyConverter.cs
+++ OpenCover2Generic.Converter/MultiAssemblyConverter.cs
@@ -13,12 +13,15 @@ namespace BHGE.SonarQube.OpenCover2Generic
         private readonly IModel _model;
         private readonly ICoverageWriter _coverageWriter;
         private readonly ICoverageParser _parser;
-
+        private readonly ICoverageParser _moduleParser;
+        private readonly ICoverageWriter _moduleWriter;
         public MultiAssemblyConverter(IModel model,ICoverageParser parser,ICoverageWriter coverageWriter)
         {
             _parser = parser;
             _model = model;
             _coverageWriter = coverageWriter;
+            _moduleParser = new OpenCoverCoverageParser();
+            _moduleWriter = new OpenCoverCoverageWriter();
         }
 
         public void Convert(StreamWriter writer, StreamReader reader)
@@ -31,6 +34,10 @@ namespace BHGE.SonarQube.OpenCover2Generic
                     xmlReader.MoveToContent();
                     while (_parser.ParseModule(_model,xmlReader))
                     {
+                        string moduleFile = Path.GetTempFileName();
+                        WriteModuleToFile(moduleFile);
+                        _model.Clear();
+                        ReadModuleFromFile(moduleFile);
                         _coverageWriter.GenerateCoverage(_model, xmlWriter);
                         _model.Clear();
                     };
@@ -39,6 +46,25 @@ namespace BHGE.SonarQube.OpenCover2Generic
                 }
                 _coverageWriter.WriteEnd(xmlWriter);
             }
+        }
+
+        private void ReadModuleFromFile(string moduleFile)
+        {
+            using (XmlReader tempFileReader = XmlReader.Create(moduleFile))
+            {
+                tempFileReader.MoveToContent();
+                _moduleParser.ParseModule(_model, tempFileReader);
+            }
+        }
+
+        private void WriteModuleToFile(string moduleFile)
+        {
+            using (XmlWriter tempFileWriter = new XmlTextWriter(moduleFile, Encoding.UTF8))
+            {
+                //write it to the temp file
+                _moduleWriter.GenerateCoverage(_model, tempFileWriter);
+            }
+
         }
     }
 }
