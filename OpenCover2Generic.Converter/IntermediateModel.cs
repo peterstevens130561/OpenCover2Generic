0701e3763cd760343f300c77be97567f0471f9d5
Peter Stevens <peter.stevens@bakerhughes.com>
2017-11-02 16:22:56 +0100

    Failing test

---
+++ OpenCover2Generic.Converter/IntermediateModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    /// <summary>
+    /// Used to create a model from multiple coverage files. Though they pertain to the same module, each one may have different
+    /// numbering for the files. Therefore,  there is small intermediate step
+    /// </summary>
+    class IntermediateModel : IModel
+    {
+
+        public void AddBranchPoint(int fileId, int sourceLine, int path, bool isVisited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFile(string fileId, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSequencePoint(string fileId, string sourceLine, string visitedCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ISourceFileCoverageModel> GetCoverage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenCover2Generic.UnitTests/IntermediateModelTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    [TestClass]
+    public class IntermediateModelTests
+    {
+        [TestMethod]
+        public void AddOneFile()
+        {
+            var intermediateModel = new IntermediateModel();
+            intermediateModel.AddFile("1", "a");
+            Assert.AreEqual(1, intermediateModel.GetCoverage().Count);
+            Assert.AreEqual("a", intermediateModel.GetCoverage()[0].FullPath);
+        }
+
+        [TestMethod]
+        public void AddSecondFileWithPathAlreadyAdded()
+        {
+            var intermediateModel = new IntermediateModel();
+            intermediateModel.AddFile("1", "a");
+            intermediateModel.AddFile("2", "a");
+            Assert.AreEqual(1, intermediateModel.GetCoverage().Count);
+            Assert.AreEqual("a", intermediateModel.GetCoverage()[0].FullPath);
+            Assert.AreEqual("1", intermediateModel.GetCoverage()[0].Uid);
+        }
+
+    }
+}
