998b476d353b716b6dad1e44ccef519e7ff78e3e
Peter Stevens <peter.stevens@bakerhughes.com>
2017-11-07 10:17:08 +0100

    Add failing test

---
+++ OpenCover2Generic.UnitTests/ProcessFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    [TestClass]
+    class ProcessFactoryTests
+    {
+
+        [TestMethod]
+        public void InstantiationTest() {
+            Process process = new ProcessFactory().CreateProcess();
+        }
+
+    }
+}
