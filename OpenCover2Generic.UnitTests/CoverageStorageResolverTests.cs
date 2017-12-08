0e38d9ee81b9688f2dfaee4ba31a93b82de14e0f
Peter Stevens <peter.stevens@bakerhughes.com>
2017-12-08 10:42:45 +0530

    First commit for CoverageStorageResolver

---
+++ OpenCover2Generic.UnitTests/CoverageStorageResolverTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    [TestClass]
+    public class CoverageStorageResolverTests
+    {
+
+        private ICoverageStorageResolver _resolver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetPathForModule_Valid_ProperLocation()
+        {
+            
+        }
+    }
+}
