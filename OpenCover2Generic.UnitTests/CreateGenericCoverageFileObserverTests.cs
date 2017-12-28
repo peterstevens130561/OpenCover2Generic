dfc6caf52bf64a3952e06932c47e63996e1fafa9
Peter Stevens <peter.stevens@bakerhughes.com>
2017-12-28 10:40:28 +0100

    failing test

--- OpenCover2Generic.UnitTests/CreateGenericCoverageFileObserverTests.cs
+++ OpenCover2Generic.UnitTests/CreateGenericCoverageFileObserverTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BHGE.SonarQube.OpenCover2Generic.Repositories.Coverage;
+using BHGE.SonarQube.OpenCover2Generic.Writers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BHGE.SonarQube.OpenCover2Generic
@@ -15,7 +16,7 @@ namespace BHGE.SonarQube.OpenCover2Generic
         [TestInitialize]
         public void Initialize()
         {
-            
+            observer = new GenericCoverageFileWriterObserver();
         }
     }
 }
