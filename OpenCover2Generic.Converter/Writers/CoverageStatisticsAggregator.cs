425b5d015817e90b5dd3c20d361d4b7594507aaa
Peter Stevens <peter.stevens@bakerhughes.com>
2017-12-28 18:00:39 +0100

    Step

---
+++ OpenCover2Generic.Converter/Writers/CoverageStatisticsAggregator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.Repositories.Coverage;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Writers
+{
+    class CoverageStatisticsAggregator : ICoverageStatisticsAggregator, IScannerObserver
+    {
+        public void OnBeginScan(object sender, EventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEndScan(object sender, EventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnModule(object v, ModuleEventArgs moduleEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenCover2Generic.UnitTests/CoverageStatisticsObserverTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.Writers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BHGE.SonarQube.OpenCover2Generic
+{
+    [TestClass]
+    public class CoverageStatisticsObserverTests
+    {
+        private ICoverageStatisticsAggregator observer;
+        [TestInitialize]
+        public void Initialize()
+        {
+            observer = new CoverageStatisticsAggregator();
+        }
+    }
+}
