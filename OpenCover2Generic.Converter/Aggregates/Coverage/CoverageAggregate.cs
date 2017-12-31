b1cfad6c6e1391fce106261fb43bba9d74c0acb9
Peter Stevens <peter.stevens@bakerhughes.com>
2017-12-31 17:00:10 +0100

    Test passes

--- OpenCover2Generic.Converter/Aggregates/Coverage/CoverageAggregate.cs
+++ OpenCover2Generic.Converter/Aggregates/Coverage/CoverageAggregate.cs
@@ -8,13 +8,14 @@ namespace BHGE.SonarQube.OpenCover2Generic.Aggregates.Coverage
 {
     public class CoverageAggregate : ICoverageAggregate
     {
-        private string key;
-        private string path;
 
         public CoverageAggregate(string path, string key)
         {
-            this.path = path;
-            this.key = key;
+            Path = path;
+            Key = key;
         }
+
+        public string Key { get; private set; }
+        public string Path { get; private set; }
     }
 }
