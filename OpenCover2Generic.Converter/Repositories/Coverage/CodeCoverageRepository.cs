1e1478360899becff84ae895c72cf927b1f9d577
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-03 16:08:33 +0100

    Removed redundant code

--- OpenCover2Generic.Converter/Repositories/Coverage/CodeCoverageRepository.cs
+++ OpenCover2Generic.Converter/Repositories/Coverage/CodeCoverageRepository.cs
@@ -61,60 +61,6 @@ namespace BHGE.SonarQube.OpenCover2Generic.Repositories.Coverage
         }
 
         /// <summary>
-        /// Add a coverage output file of OpenCover to the repository
-        /// </summary>
-        /// <param name="path">to the openCover file</param>
-        /// <param name="key">some unique key</param>
-        private void Add(string path, string key)
-        {
-            lock (_lock)
-            {
-                _parser = new OpenCoverCoverageParser();
-                _moduleWriter = new OpenCoverCoverageWriter();
-                try
-                {
-                    using (var xmlReader = XmlReader.Create(path))
-                    {
-                        xmlReader.MoveToContent();
-                        _model = new IntermediateModel();
-                        while (_parser.ParseModule(_model, xmlReader))
-                        {
-                            WriteModule(RootDirectory, key);
-                        }
-                        WriteModule(RootDirectory, key);
-                    }
-
-                }
-                catch (Exception e)
-                {
-                    _log.Error($"Exception thrown during reading {path}\n{e.Message}\n{e.StackTrace}");
-                    throw;
-                }
-            }
-        }
-
-        private void WriteModule(string rootPath, string key)
-        {
-            if (_model.GetSourceFiles().Count > 0)
-            {
-                string moduleFile = _coverageStorageResolver.GetPathForAssembly(rootPath, _parser.ModuleName,key);
-                WriteModuleToFile(_model,moduleFile);
-                _model.Clear();
-            }
-        }
-
-        private void WriteModuleToFile(IModuleCoverageModel model,string moduleFile)
-        {
-            using (XmlTextWriter tempFileWriter = new XmlTextWriter(moduleFile, Encoding.UTF8))
-            {
-                //write it to the temp file
-                _moduleWriter.WriteBegin(tempFileWriter);
-                _moduleWriter.GenerateCoverage(model, tempFileWriter);
-                _moduleWriter.WriteEnd(tempFileWriter);
-            }
-        }
-
-        /// <summary>
         /// provides an observable fluent query of the coverage information by module.
         /// As the repository can be many GB, this query should be executed once, first registering all
         /// observers, then executing
