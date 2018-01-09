592830da99d58142c71e5d06f9db8b27563f467a
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-09 14:51:43 +0100

    And even cleaner

--- OpenCover2Generic.Converter/Application/Services/Workspace/WorkspaceService.cs
+++ OpenCover2Generic.Converter/Application/Services/Workspace/WorkspaceService.cs
@@ -10,10 +10,6 @@ namespace BHGE.SonarQube.OpenCover2Generic.Application.Services.Workspace
 {
     class WorkspaceService: IWorkspaceService
     {
-        public WorkspaceService() 
-        {
-            
-        }
         public string Id { get;set; }
     }
 }
