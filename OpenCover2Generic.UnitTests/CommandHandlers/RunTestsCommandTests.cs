ea938c8a4e04beac80e89ff979f311bb1e12624d
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-10 18:10:43 +0100

    Start writing failing test

--- OpenCover2Generic.UnitTests/CommandHandlers/RunTestsCommandTests.cs
+++ OpenCover2Generic.UnitTests/CommandHandlers/RunTestsCommandTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.Application.Commands.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BHGE.SonarQube.OpenCover2Generic.CommandHandlers
@@ -13,7 +14,8 @@ namespace BHGE.SonarQube.OpenCover2Generic.CommandHandlers
         [TestMethod]
         public void RunTests_NoAssemblies_RunTests_CompletesFine()
         {
-            
+            IRunTestsCommand runTestsCommand;
+
         }
     }
 }
