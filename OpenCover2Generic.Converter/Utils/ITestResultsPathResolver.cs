5299f1354b7e0fec65dddb3635f06b9f0369a241
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-16 13:38:54 -0600

    One more test passes

--- OpenCover2Generic.Converter/Utils/ITestResultsPathResolver.cs
+++ OpenCover2Generic.Converter/Utils/ITestResultsPathResolver.cs
@@ -36,6 +36,27 @@ namespace BHGE.SonarQube.OpenCover2Generic.Utils
             return path;
         }
 
+        private readonly Dictionary<string, int> _assemblyUsageLookupTable = new Dictionary<string, int>();
+        private readonly Object _lock = new object();
+        private string GetIndex(string key)
+        {
+            string index;
+            lock (_lock)
+            {
+                if (!_assemblyUsageLookupTable.ContainsKey(key))
+                {
+                    _assemblyUsageLookupTable.Add(key, _assemblyUsageLookupTable.Count + 1);
+                }
+                index = _assemblyUsageLookupTable[key].ToString();
+            }
+            return index;
+        }
+
+        private string GetFileForAssembly(string basePath, string assemblyPath, string extension)
+        {
+            string index = GetIndex(assemblyPath);
+            return Path.Combine(basePath, index + "_" + Path.GetFileNameWithoutExtension(assemblyPath) + "." + extension);
+        }
         public string GetTestResultsDestinationPath(string v)
         {
             throw new NotImplementedException();
@@ -43,7 +64,7 @@ namespace BHGE.SonarQube.OpenCover2Generic.Utils
 
         public IEnumerable<string> GetTestResultsPaths()
         {
-            throw new NotImplementedException();
+            return _fileSystemAdapter.EnumerateFiles(GetDirectory());
         }
     }
 }
