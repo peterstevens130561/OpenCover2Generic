7184a790242c8cc7019f96baf61db8791a076e56
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-17 12:45:38 -0600

    first stuff

---
+++ OpenCover2Generic.Converter/Application/Commands/CoverageResultsCreate/CreateCoverageResultsCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.DomainModel.Workspace;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Application.Commands.CoverageResultsCreate
+{
+    class CreateCoverageResultsCommand : ICreateCoverageResultsCommand
+    {
+        public IWorkspace Workspace { get; set; }
+        public string[] Args { get; set; }
+    }
+}
---
+++ OpenCover2Generic.UnitTests/CommandHandlers/CreateCoverageResultsCommandHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.Application.Commands.CoverageResultsCreate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BHGE.SonarQube.OpenCover2Generic.CommandHandlers
+{
+    [TestClass]
+    public class CreateCoverageResultsCommandHandlerTests
+    {
+        private readonly CreateCoverageResultsCommandHandler _commandHandler;
+        [TestMethod]
+        public void Execute_Normal_Execute_DepsCalled()
+        {
+            ICreateCoverageResultsCommand command = new CreateCoverageResultsCommand();
+  
+        }
+    }
+}
