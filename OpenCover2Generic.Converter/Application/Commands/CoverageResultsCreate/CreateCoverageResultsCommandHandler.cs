5b685d4bfc3e3bd2fe5e0933105c4a18e1117e5b
Peter Stevens <peter.stevens@bakerhughes.com>
2018-01-18 13:19:16 -0600

    Time for a commit

---
+++ OpenCover2Generic.Converter/Application/Commands/CoverageResultsCreate/CreateCoverageResultsCommandHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using BHGE.SonarQube.OpenCover2Generic.CQRS.CommandBus;
+using BHGE.SonarQube.OpenCover2Generic.Repositories.Coverage;
+using BHGE.SonarQube.OpenCoverWrapper;
+using BHGE.SonarQube.OpenCover2Generic.Writers;
+using log4net;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Application.Commands.CoverageResultsCreate
+{
+    class CreateCoverageResultsCommandHandler : ICommandHandler<ICreateCoverageResultsCommand>
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(CreateCoverageResultsCommandHandler));
+        private readonly ICodeCoverageRepository _codeCoverageRepository;
+        private readonly IOpenCoverWrapperCommandLineParser _commandLineParser;
+        private readonly IQueryAllModulesResultObserver _statisticsObserver;
+        private readonly IQueryAllModulesResultObserver _genericCoverageWriterObserver;
+
+        public CreateCoverageResultsCommandHandler() : this(new OpenCoverWrapperCommandLineParser(), 
+            new CodeCoverageRepository(),
+            new GenericCoverageWriterObserver(new GenericCoverageWriter()),
+            new CoverageStatisticsAggregator()
+            )
+        {
+            
+        }
+        public CreateCoverageResultsCommandHandler(IOpenCoverWrapperCommandLineParser openCoverWrapperCommandLineParser, 
+            ICodeCoverageRepository codeCoverageRepository,
+            IQueryAllModulesResultObserver genericCoverageWriterObserver,
+            IQueryAllModulesResultObserver statisticsObserver)
+        {
+            _commandLineParser = openCoverWrapperCommandLineParser;
+            _codeCoverageRepository = codeCoverageRepository;
+            _statisticsObserver = statisticsObserver;
+            _genericCoverageWriterObserver = genericCoverageWriterObserver;
+        }
+
+        public void Execute(ICreateCoverageResultsCommand command)
+        {
+
+            _commandLineParser.Args = command.Args;
+            _codeCoverageRepository.Workspace = command.Workspace;
+
+            string outputPath = _commandLineParser.GetOutputPath();
+
+            using (var writer = new XmlTextWriter(outputPath, Encoding.UTF8))
+            {
+               ((IGenericCoverageWriterObserver)_genericCoverageWriterObserver).Writer = writer;
+                _codeCoverageRepository.QueryAllModules()
+                    .AddObserver(_genericCoverageWriterObserver)
+                    .AddObserver(_statisticsObserver)
+                    .Execute();
+            }
+            _log.Info($"Files         : {statisticsObserver.Files}");
+            _log.Info($"Lines         : {statisticsObserver.Lines} ");
+            _log.Info($"Covered Lines : {statisticsObserver.CoveredLines}");
+        }
+    }
+}
--- OpenCover2Generic.UnitTests/CommandHandlers/CreateCoverageResultsCommandHandlerTests.cs
+++ OpenCover2Generic.UnitTests/CommandHandlers/CreateCoverageResultsCommandHandlerTests.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BHGE.SonarQube.OpenCover2Generic.Application.Commands.CoverageResultsCreate;
+using BHGE.SonarQube.OpenCover2Generic.DomainModel.Workspace;
+using BHGE.SonarQube.OpenCover2Generic.Repositories.Coverage;
+using BHGE.SonarQube.OpenCoverWrapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BHGE.SonarQube.OpenCover2Generic.CommandHandlers
 {
     [TestClass]
     public class CreateCoverageResultsCommandHandlerTests
     {
-        private readonly CreateCoverageResultsCommandHandler _commandHandler;
+        private CreateCoverageResultsCommandHandler _commandHandler;
+        private Mock<ICodeCoverageRepository> _repositoryMock;
         [TestMethod]
         public void Execute_Normal_Execute_DepsCalled()
         {
             ICreateCoverageResultsCommand command = new CreateCoverageResultsCommand();
-  
+            _repositoryMock=new Mock<ICodeCoverageRepository>();
+
+            command.Args = new[] {"-output:opencover.xml"};
+            command.Workspace = new Workspace("jadieda");
+            _commandHandler = new CreateCoverageResultsCommandHandler();
+           
         }
     }
 }
