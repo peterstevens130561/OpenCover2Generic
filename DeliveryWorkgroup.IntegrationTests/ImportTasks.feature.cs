86a0924bce770a51e0440d7eb860c53cfdbb030b
Peter Stevens <peter.stevens@bakerhughes.com>
2018-07-02 09:57:35 +0200

    Removed deliveryworkgroup crap

--- DeliveryWorkgroup.IntegrationTests/ImportTasks.feature.cs
+++
@@ -1,6 +0,0 @@
-﻿#error Parsing error near 'Scenario : someproject'
-#error Steps can only be specified for scenarios or scenario outlines.
-#error Steps can only be specified for scenarios or scenario outlines.
-#error Steps can only be specified for scenarios or scenario outlines.
-#error Steps can only be specified for scenarios or scenario outlines.
-#error Steps can only be specified for scenarios or scenario outlines.
