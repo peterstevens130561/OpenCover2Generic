1d532ba27400ee4f641223f22b1721496c142861
Peter Stevens <peter.stevens@bakerhughes.com>
2018-07-03 09:59:36 +0200

    Adding missing files

---
+++ OpenCover2Generic.Converter/Application/Commands/TestResultsCreate/TestResultsCreateCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.DomainModel.Workspace;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Application.Commands.TestResultsCreate
+{
+    public class TestResultsCreateCommand : ITestResultsCreateCommand
+    {
+        public string[] Args { get; set; }
+
+        public IWorkspace Workspace { get; set; }
+    }
+}
---
+++ OpenCover2Generic.Converter/Application/Commands/TestResultsCreate/TestResultsCreateCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHGE.SonarQube.OpenCover2Generic.CQRS.CommandBus;
+using BHGE.SonarQube.OpenCover2Generic.Repositories.Tests;
+using BHGE.SonarQube.OpenCoverWrapper;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Application.Commands.TestResultsCreate
+{
+    class TestResultsCreateCommandHandler : ICommandHandler<ITestResultsCreateCommand>
+    {
+        private readonly ITestResultsRepository _testResultsRepository;
+        private readonly IOpenCoverWrapperCommandLineParser _commandLineParser;
+
+        public TestResultsCreateCommandHandler() : this(
+            new TestResultsRepository(),
+            new OpenCoverWrapperCommandLineParser())
+        {
+            
+        }
+
+        public TestResultsCreateCommandHandler(
+            ITestResultsRepository testResultsRepository, 
+            IOpenCoverWrapperCommandLineParser commandLineParser)
+        {
+            _testResultsRepository = testResultsRepository;
+            _commandLineParser = commandLineParser;
+        }
+
+        public void Execute(ITestResultsCreateCommand command)
+        {
+            _commandLineParser.Args = command.Args;
+            _testResultsRepository.SetWorkspace(command.Workspace);
+
+            string testResultsPath = _commandLineParser.GetTestResultsPath();
+            _testResultsRepository.Write(testResultsPath);
+        }
+    }
+}
