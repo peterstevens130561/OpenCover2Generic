8dd87e556d879d1acd7debed6dd6a64132808139
Peter Stevens <peter.stevens@bakerhughes.com>
2018-07-03 09:59:36 +0200

    Adding missing files

---
+++ OpenCover2Generic.Converter/Application/Commands/TestResultsCreate/ITestResultsCreateCommand.cs
@@ -0,0 +1,11 @@
+﻿using BHGE.SonarQube.OpenCover2Generic.CQRS.CommandBus;
+using BHGE.SonarQube.OpenCover2Generic.DomainModel.Workspace;
+
+namespace BHGE.SonarQube.OpenCover2Generic.Application.Commands.TestResultsCreate
+{
+    public interface ITestResultsCreateCommand : ICommand
+    {
+        string[] Args { get; set; }
+        IWorkspace Workspace { get; set; }
+    }
+}
